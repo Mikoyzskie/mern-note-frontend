fadfe2eb17736a0d3779d6f970ea9744af24ce04
Mikoy <rapidstrike13@gmail.com>
2023-09-16 23:36:25 +0800

    add login and register components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
 import NoteContainer from './components/NotesContainer';
 import './App.scss';
-
-
 import Sidebar from "./components/Sidebar";
 import { ModalProvider } from './lib/Providers';
+import Login from './components/Login';
+import Register from "./components/Register";
 
 function App() {
   return (
-    <ModalProvider>
-      <div className='flex'>
-        <Sidebar />
+    // <ModalProvider>
+    //   <div className='flex'>
+    //     <Sidebar />
 
-        <NoteContainer />
-      </div>
-    </ModalProvider>
-
+    //     <NoteContainer />
+    //   </div>
+    // </ModalProvider>
+    // <Login />
+    <Register />
   );
 }
 
